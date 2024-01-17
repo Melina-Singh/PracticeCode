5c3141e22f5b40f2d355fc49e563e7e69c1682ec
Melina-Singh <85394677+Melina-Singh@users.noreply.github.com>
2024-01-17 09:54:15 +0545

    Update funct_day1.py

--- funct_day1.py
+++ funct_day1.py
@@ -1,15 +1,1 @@
-# Defining a Functions
 
-def greet_user():
-    """Display a simple greeting"""
-    print("Hello Melina")
-
-greet_user() #this is called calling the function
-
-# PAsssing Information to a Function 
-def greet(username): # username vaneko function ko parameter ho
-    print("Hello "+ username.title()+ "!")
-
-greet('JEssica')
-
-    
