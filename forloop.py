57e121551296de23e646592a462e99757f2416fc
Melina <85394677+Melina-Singh@users.noreply.github.com>
2024-03-26 21:58:47 +0545

    basicpatterns python

---
+++ forloop.py
@@ -0,0 +1,40 @@
+# pattern solving
+# 1. size (How many rows you want in the pattern)
+
+n = 5                     #square pattern
+# print("*"*  n)
+for i in range(n): #rows
+    for i in range(n):  #columns
+        print("$", end ='  ')
+    print()
+
+print("\nThe next pattern is decreasing triangle pattern\n")
+
+
+n = 5
+for i in range(n):
+    for j in range(i,n):
+        print("$", end='  ')
+    print()
+
+print("\nThis is now the increasing triangle pattern\n")
+n = 5
+for i in range(n):
+    for j in range(i+1):
+        print("$", end='  ')
+    print()
+
+
+# Right sided triangle
+
+
+n = int(input("Enter the number of Rows: "))
+for i in range(n):
+    for j in range(i, n):
+        print(' ', end=' ')
+    for j in range(i + 1):
+        print("$", end=' ')
+    print()
+
+
+
---
+++ hill.py
@@ -0,0 +1,21 @@
+n = 5
+for i in range(n):
+
+    for j in range(i, n):
+        print(' ', end=' ')
+    for j in range(1,i):
+        print("$", end=' ')
+    for j in range(i+1):
+        print("$", end=' ')
+        
+    print()
+
+
+#  diamond pattern
+n = 5  
+
+for i in range(n):
+    print(' ' * (n - i - 1) + '*' * (2 * i + 1))
+
+for i in range(n - 2, -1, -1):
+    print(' ' * (n - i - 1) + '*' * (2 * i + 1))
