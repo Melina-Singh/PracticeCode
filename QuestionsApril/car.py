e58e7892d1840074749336c897afe849577e6d2b
Melina <85394677+Melina-Singh@users.noreply.github.com>
2024-04-03 22:04:09 +0545

    Update car.py

--- QuestionsApril/car.py
+++ QuestionsApril/car.py
@@ -1,7 +1,7 @@
 class Car():
     def __init__(self, make, model, year):
         self.make = make
-        self.model = model
+        self.model = model 
         self.year = year
         self.odometer_reading = 0
 
