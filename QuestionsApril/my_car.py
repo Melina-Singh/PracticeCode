b9821f4b87dff2a508b98df724b1160355cb186f
Melina <85394677+Melina-Singh@users.noreply.github.com>
2024-04-03 22:04:37 +0545

    Update my_car.py

--- QuestionsApril/my_car.py
+++ QuestionsApril/my_car.py
@@ -8,7 +8,7 @@ print(my_new_car.get_descriptive_name())
 
 my_new_car.odometer_reading = 23
 my_new_car.read_odometer()
-
+ 
 myTesla = ElectricCar("Tesla", 'model s', 2016)
 print(myTesla.get_descriptive_name())
 print(myTesla.battery.describe_battery())
